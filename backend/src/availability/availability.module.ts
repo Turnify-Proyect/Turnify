0124e7758845652fc984af172457d3011cb34783
Valeriat92 <valeriatrevisi7@gmail.com>
2026-09-06 22:17:47 -0300

    Trabajando en Appointments

--- backend/src/availability/availability.module.ts
+++ backend/src/availability/availability.module.ts
@@ -3,9 +3,10 @@ import { AvailabilityService } from './availability.service';
 import { AvailabilityController } from './availability.controller';
 import { Availability } from './entities/availability.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Professional } from 'src/professionals/entities/professional.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Availability])],
+  imports: [TypeOrmModule.forFeature([Availability, Professional])],
   controllers: [AvailabilityController],
   providers: [AvailabilityService],
 })
--- backend/src/availability/dto/create-availability.dto.ts
+++ backend/src/availability/dto/create-availability.dto.ts
@@ -1,1 +1,26 @@
-export class CreateAvailabilityDto {}
+import {IsEnum, IsNotEmpty, IsString, IsUUID, Matches,} from 'class-validator';
+import { DayOfWeek } from '../entities/availability.entity';
+
+export class CreateAvailabilityDto {
+  @IsUUID()
+  @IsNotEmpty()
+  professionalId!: string;
+
+  @IsEnum(DayOfWeek)
+  @IsNotEmpty()
+  dayOfWeek!: DayOfWeek;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+    message: 'startTime debe tener formato HH:mm',
+  })
+  startTime!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+    message: 'endTime debe tener formato HH:mm',
+  })
+  endTime!: string;
+}
